b16dc0bda1e02990e4bd7ede33634234060df3b7
Yasin Şükrü Tan <hi@mobita.co>
2022-11-17 10:32:38 +0300

    Cookcademy xcode project created

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Yasin Şükrü Tan on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
