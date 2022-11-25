8fb25dd3b631c9918006e8b7d6a31657ad4d33e9
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-25 20:57:00 +0300

    View groups are created

---
+++ Cookcademy/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyRecipeView.swift
+//  Cookcademy
+//
+//  Created by Yasin Şükrü Tan on 25.11.2022.
+//
+
+import SwiftUI
+
+struct ModifyRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModifyRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModifyRecipeView()
+    }
+}
