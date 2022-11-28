b010e4ff6650ec4452e05f6902c7b6a039229f39
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-28 22:19:34 +0300

    New views created and new init funcs for ingredients are created

--- Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct ModifyIngredientView: View {
+    @Binding var ingredient: Ingredient
+    let createAction: ((Ingredient) -> Void)
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    @Environment(\.presentationMode) private var mode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Ingredient Name", text: $ingredient.name)
+                .listRowBackground(listBackgroundColor)
+            Stepper(value: $ingredient.quantity, in: 0...100, step: 0.5) {
+                HStack {
+                    Text("Quantity:")
+                    TextField("Quantity", value: $ingredient.quantity, formatter: NumberFormatter.decimal)
+                        .keyboardType(.numbersAndPunctuation)
+                }
+            }.listRowBackground(listBackgroundColor)
+            Picker(selection: $ingredient.unit, label: HStack {
+                Text("Unit")
+                Spacer()
+                Text(ingredient.unit.rawValue)
+            }) {
+                ForEach(Ingredient.Unit.allCases, id: \.self) { unit in
+                    Text(unit.rawValue)
+                }
+            }
+            .listRowBackground(listBackgroundColor)
+            .pickerStyle(MenuPickerStyle())
+            HStack {
+                Spacer()
+                Button("Save") {
+                    createAction(ingredient)
+                    mode.wrappedValue.dismiss()
+                }
+                Spacer()
+            }.listRowBackground(listBackgroundColor)
+        }
+        .foregroundColor(listTextColor)
+    }
+}
+
+extension NumberFormatter {
+    static var decimal: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
     }
 }
 
 struct ModifyIngredientView_Previews: PreviewProvider {
+    @State static var recipe = Recipe.testRecipes[0]
     static var previews: some View {
-        ModifyIngredientView()
+        NavigationView {
+            ModifyIngredientView(ingredient: $recipe.ingredients[0]) { ingredient in
+                print(ingredient)
+            }.navigationTitle("Add Ingredient")
+        }
     }
 }
--- Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ModifyMainInformationView: View {
+    @Binding var mainInformation: MainInformation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Recipe Name", text: $mainInformation.name)
+            TextField("Author", text: $mainInformation.author)
+            Section(header: Text("Description")) {
+                TextEditor(text: $mainInformation.description)
+            }
+            Picker(selection: $mainInformation.category, label:
+                    HStack {
+                Text("Category")
+                Spacer()
+                Text(mainInformation.category.rawValue)
+            }) {
+                ForEach(MainInformation.RecipeCategory.allCases,
+                        id: \.self) { category in
+                    Text(category.rawValue)
+                }
+            }
+            .pickerStyle(MenuPickerStyle())
+        }
     }
 }
 
 struct ModifyMainInformationView_Previews: PreviewProvider {
+    @State static var mainInformation = MainInformation(name: "Test Name",
+                                                        description: "Test Description",
+                                                        author: "Test Author",
+                                                        category: .breakfast)
+    @State static var emptyInformation = MainInformation(name: "", description: "", author: "", category: .breakfast)
+
     static var previews: some View {
-        ModifyMainInformationView()
+        ModifyMainInformationView(mainInformation: $mainInformation)
+        ModifyMainInformationView(mainInformation: $emptyInformation)
     }
 }
