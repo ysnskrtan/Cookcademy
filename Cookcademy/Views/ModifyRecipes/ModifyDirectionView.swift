ba3225581cf79ac9bcc3ee5801b55b07d609e133
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-01 21:38:47 +0300

    Ingredients view renamed to Components view, Direction view file is created

---
+++ Cookcademy/Views/ModifyRecipes/ModifyDirectionView.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyDirectionView.swift
+//  Cookcademy
+//
+//  Created by Yasin Şükrü Tan on 1.12.2022.
+//
+
+import SwiftUI
+
+struct ModifyDirectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModifyDirectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModifyDirectionView()
+    }
+}
