0a2acf521ad3d36c3b86aa4eb1d739786cfc79ec
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-05 20:45:26 +0300

    Settings view created

---
+++ Cookcademy/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Cookcademy
+//
+//  Created by Yasin Şükrü Tan on 5.12.2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
